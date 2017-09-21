2003dfe34a988df6486860615a1e529898e82836
haoqing <haoqing_007@163.com>
2017-09-21 18:03:10 +0800

    modify login formmodule

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,19 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../core/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [AuthService]
+
 })
 export class LoginComponent implements OnInit {
+  username = '';
+  password = '';
 
-  constructor() { }
+  // 在构造函数中将AuthService实例注入到成员变量service中
+  constructor(private service: AuthService) { }
 
   ngOnInit() {
   }
 
-  onClick(){
-    console.log('button was clicked');
+
+  onSubmit(formValue) {
+    console.log(formValue);
+    console.log('auth result is : ' + this.service.loginWithCredentials(this.username, this.password));
   }
 
 }
